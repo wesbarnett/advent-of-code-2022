c049e38bf506ad8b182f36797dba52758b615de8
Wes Barnett <wes@barnettphd.com>
2022-12-09 17:00:04 -0500

    day 1 part 1

---
+++ day1/main.py
@@ -0,0 +1,12 @@
+from argparse import ArgumentParser
+from pathlib import Path
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-i", "--infile", required=False, type=Path, default=Path("input")
+    )
+    args = parser.parse_args()
+
+    lines = Path(args.infile).read_text().rstrip("\n").split("\n\n")
+    print(max([sum(int(z) for z in y) for y in [x.split("\n") for x in lines]]))
