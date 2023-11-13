4a67ff965a11ad7a9c8fe8f268a45a34cdf2d219
Wes Barnett <wes@barnettphd.com>
2023-11-12 21:49:49 -0500

    create library

---
+++ aoc/__init__.py
@@ -0,0 +1,27 @@
+import os
+from pathlib import Path
+from urllib import parse, request
+
+
+def get_input(year, day):
+    p = Path(f"aoc_infile_{year}_{day}")
+    try:
+        aoc_input = p.read_text()
+    except FileNotFoundError:
+        url = f"https://adventofcode.com/{year}/day/{day}/input"
+        headers = {"Cookie": f"session={os.environ['AOC_COOKIE']}"}
+        req = request.Request(url, headers=headers)
+        with request.urlopen(req) as response:
+            aoc_input = response.read().decode("utf-8")
+            p.write_text(aoc_input)
+    return aoc_input
+
+
+def submit(answer, year, day, level):
+    url = f"https://adventofcode.com/{year}/day/{day}/answer"
+    headers = {"Cookie": f"session={os.environ['AOC_COOKIE']}"}
+    req = request.Request(url, headers=headers)
+    data = parse.urlencode({"level": level, "answer": answer}).encode()
+    req = request.Request(url, data=data, headers=headers)
+    with request.urlopen(req) as response:
+        print(response.read().decode("utf-8"))
