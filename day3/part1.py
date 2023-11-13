6729c0a7fc76feb97c58e573dd4291c516225ae9
Wes Barnett <wes@barnettphd.com>
2023-11-13 07:03:57 -0500

    add day 3 part 1

---
+++ day3/part1.py
@@ -0,0 +1,26 @@
+import string
+
+from aoc import get_input, submit
+
+if __name__ == "__main__":
+    year, day, level = 2022, 3, 1
+    aoc_input = get_input(year, day)
+
+    lines = aoc_input.rstrip("\n").split("\n")
+
+    total_priority = 0
+
+    priority = {
+        **{x: i for i, x in enumerate(string.ascii_lowercase, start=1)},
+        **{x: i for i, x in enumerate(string.ascii_uppercase, start=27)},
+    }
+
+    for line in lines:
+        mid = len(line) // 2
+        first, sec = set(line[:mid]), set(line[mid:])
+        dup = first.intersection(sec)
+        total_priority += priority[str(*dup)]
+
+    print(total_priority)
+
+    submit(total_priority, year, day, level)
