59ad54c8f9c98f93be824f8a8e16d29139163b92
Wes Barnett <wes@barnettphd.com>
2023-11-14 06:59:57 -0500

    add initial population for day 7 part 1

---
+++ day7/part1.py
@@ -0,0 +1,58 @@
+from aoc import get_input  # , submit
+
+
+class Node:
+    def __init__(self, name, parent=None, size=0):
+        self.name = name
+        self.files = []
+        self.dirs = []
+        self.parent = parent
+        self.size = size
+
+    def __repr__(self):
+        return f"Node(name={self.name},size={self.size})"
+
+    def add_dir(self, dirname, parent):
+        for d in self.dirs:
+            if d.name == dirname:
+                print("dir already added")
+                return
+        self.dirs.append(Node(dirname, parent))
+
+    def add_file(self, filename, parent, size):
+        for f in self.files:
+            if f.name == filename:
+                print("file already added")
+                return
+        self.files.append(Node(filename, parent, size))
+
+
+if __name__ == "__main__":
+    year, day, level = 2022, 7, 1
+    aoc_input = get_input(year, day)
+    lines = aoc_input.rstrip("\n").split("\n")
+
+    node = Node(-1)
+    for line in lines:
+        if line.startswith("$ cd"):
+            loc = line.split(" ")[-1]
+            if loc == "..":
+                node = node.parent
+            else:
+                # Check if node already added
+                for d in node.dirs:
+                    if d.name == loc:
+                        node = d
+                        break
+                else:
+                    node = Node(loc, parent=node)
+        elif line.startswith("$ ls"):
+            pass
+        else:
+            if line.startswith("dir"):
+                node.add_dir(line.split(" ")[-1], parent=node)
+            else:
+                size, name = line.split(" ")
+                node.add_file(name, parent=node, size=size)
+
+    # submit(ans, year, day, level)
