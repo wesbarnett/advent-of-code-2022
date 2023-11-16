339a3099e1e2666213385e56ab26f98e9b1e2eff
Wes Barnett <wes@barnettphd.com>
2023-11-16 08:43:27 -0500

    day 15 part 2

--- day15/part2.py
+++ day15/part2.py
@@ -9,21 +9,6 @@ def dist(p1, p2):
 if __name__ == "__main__":
     year, day, level = 2022, 15, 2
     aoc_input = get_input(year, day)
-    aoc_input = """Sensor at x=2, y=18: closest beacon is at x=-2, y=15
-Sensor at x=9, y=16: closest beacon is at x=10, y=16
-Sensor at x=13, y=2: closest beacon is at x=15, y=3
-Sensor at x=12, y=14: closest beacon is at x=10, y=16
-Sensor at x=10, y=20: closest beacon is at x=10, y=16
-Sensor at x=14, y=17: closest beacon is at x=10, y=16
-Sensor at x=8, y=7: closest beacon is at x=2, y=10
-Sensor at x=2, y=0: closest beacon is at x=2, y=10
-Sensor at x=0, y=11: closest beacon is at x=2, y=10
-Sensor at x=20, y=14: closest beacon is at x=25, y=17
-Sensor at x=17, y=20: closest beacon is at x=21, y=22
-Sensor at x=16, y=7: closest beacon is at x=15, y=3
-Sensor at x=14, y=3: closest beacon is at x=15, y=3
-Sensor at x=20, y=1: closest beacon is at x=15, y=3
-"""
 
     lines = aoc_input.rstrip("\n").split("\n")
 
@@ -39,7 +24,7 @@ Sensor at x=20, y=1: closest beacon is at x=15, y=3
         sensors.append(tuple(s))
         beacons.append(tuple(b))
 
-    maxc = 20
+    maxc = 4000000
     dists = []
     perim = set()
     # Find perimiters just outisde the range of each sensor
@@ -83,11 +68,15 @@ Sensor at x=20, y=1: closest beacon is at x=15, y=3
     # Find perimiters that are within a sensor's range. If within range
     # then that is not a possible location
     for s, d in zip(sensors, dists):
+        print(s, d, len(list(perim)))
+        if len(list(perim)) == 1:
+            break
         for p in list(perim):
             if dist(s, p) <= d:
                 perim.remove(p)
 
     b = list(perim)[0]
+    print(b)
     ans = b[0] * 4000000 + b[1]
     print(ans)
 
